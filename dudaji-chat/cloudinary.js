570dc29ad26058fa921aa3cc36360d121dfc8765
Han2412 <hoaihan0123tbcm@gmail.com>
2025-09-11 21:34:14 +0700

    feat: config cloud

---
+++ dudaji-chat/cloudinary.js
@@ -0,0 +1,11 @@
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+dotenv.config(); // load .env
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,   // từ dashboard
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
+});
+
+export default cloudinary;
