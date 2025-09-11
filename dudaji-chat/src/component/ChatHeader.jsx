80630dccc7806ae38f59bf008e97ab39256c4475
Han2412 <hoaihan0123tbcm@gmail.com>
2025-09-11 21:43:05 +0700

    fix: update ui name group and delete button leave

--- dudaji-chat/src/component/ChatHeader.jsx
+++ dudaji-chat/src/component/ChatHeader.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
-export default function Chatheader() {
+export default function ChatHeader({ roomName, roomId }) {
   return (
-    <div className="flex items-center justify-between mt-3 bg-gray-100">
-      <div className="flex items-center">
-        <img className="rounded-full w-12 h-12"></img>
-        <span>Doris Brown</span>
+    <div className="p-4 border-b bg-[#122670] flex justify-between items-center">
+      <div className="flex items-center ml-2">
+        <img
+          src={`https://api.dicebear.com/9.x/initials/svg?seed=${roomName}`}
+          alt="User"
+          className="w-12 h-12 rounded-full"
+        />
+        <span className="text-xl text-white ml-5">
+          {roomName || "No Room Selected"}
+        </span>
         <span className="ml-2 w-2 h-2 bg-green-400 rounded-full inline-block"></span>
       </div>
-      <div>
-        
-      </div>
+      <div></div>
     </div>
   );
 }
